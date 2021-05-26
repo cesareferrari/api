0e7b3acd5d55f1607449a1c8ad26a89831e7f20c
Cesare Ferrari <cesare@maryandferrari.com>
2021-05-26 13:45:07 -0400

    Add tests

--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -2,11 +2,8 @@ FactoryBot.define do
   factory :article do
     title { "Sample article" }
     content { "Sample content" }
-    # slug { "sample-article" }
-
     sequence :slug do |n|
       "sample-article-#{n}"
     end
   end
-
 end
